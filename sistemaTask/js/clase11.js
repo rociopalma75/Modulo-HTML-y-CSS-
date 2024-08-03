d42d99f3f726dafc4703ebb45780bfecb188e6c3
rociopalma75 <rociopalma75@gmail.com>
2024-08-03 14:05:45 -0300

    Sistema de tareas terminado

--- sistemaTask/js/clase11.js
+++ sistemaTask/js/clase11.js
@@ -4,23 +4,25 @@ const seccionAdvertencia = document.querySelector('.seccionAdvertencia');
 
 //Agregar una tarea
 document.getElementById('btn-add').addEventListener('click', ()=>{
-    if(txtTask.value === ''){
+    if(txtTask.value.trim() === ''){
         alert('Ingrese una tarea');
     }else{
         let itemList = document.createElement('li');
-        itemList.innerHTML = txtTask.value;
+        itemList.innerHTML = `${txtTask.value.trim()} <span><img src="./imgs/icono-cross.png" alt="eliminar" class="iconos"></span>`;
         listTask.appendChild(itemList);
     }
 
     txtTask.value = '';
 })
 
-//Cambiar a checked 
+//Cambiar a checked y Eliminar tarea
 listTask.addEventListener("click", function(e){
+    console.log(e);
     if(e.target.tagName === 'LI'){
         e.target.classList.toggle('checked');
-    }else if(e.target.tagName === 'SPAN'){
-        e.target.parentElement.remove(); //remueve el objetivo del padre
+    }else if(e.target.tagName === 'IMG'){
+        e.target.parentElement.parentElement.remove(); //remueve el objetivo del padre
     }
 })
+
 
